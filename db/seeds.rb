38acc904d0c782440721071a0e23e5a71e35a287
Madhur Luthra <71748091+mluthra01@users.noreply.github.com>
2023-02-28 15:28:44 -0800

    testing deploying without seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,31 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-ApplicationRecord.transaction do 
-    puts "Destroying tables..."
-    # Unnecessary if using `rails db:seed:replant`
-    User.destroy_all
+# ApplicationRecord.transaction do 
+#     puts "Destroying tables..."
+#     # Unnecessary if using `rails db:seed:replant`
+#     User.destroy_all
 
-    puts "Resetting primary keys..."
-    # For easy testing, so that after seeding, the first `User` has `id` of 1
-    ApplicationRecord.connection.reset_pk_sequence!('users')
+#     puts "Resetting primary keys..."
+#     # For easy testing, so that after seeding, the first `User` has `id` of 1
+#     ApplicationRecord.connection.reset_pk_sequence!('users')
 
-    puts "Creating users..."
-    # Create one user with an easy to remember name, email, and password:
-    User.create!(
-    name: 'Demo lition', 
-    email: 'demo@user.io', 
-    password: 'password'
-    )
+#     puts "Creating users..."
+#     # Create one user with an easy to remember name, email, and password:
+#     User.create!(
+#     name: 'Demo lition', 
+#     email: 'demo@user.io', 
+#     password: 'password'
+#     )
 
-    # More users
-    10.times do 
-    User.create!({
-    name: Faker::Name.name,
-    email: Faker::Internet.unique.email,
-    password: 'password'
-    }) 
-    end
+#     # More users
+#     10.times do 
+#     User.create!({
+#     name: Faker::Name.name,
+#     email: Faker::Internet.unique.email,
+#     password: 'password'
+#     }) 
+#     end
 
-    puts "Done!"
-    end
+#     puts "Done!"
+#     end
